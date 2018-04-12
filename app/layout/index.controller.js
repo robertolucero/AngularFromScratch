ab098d4d24f4254b17fe763ea7a5a1c52ef5f2d6
Roberto <robertoalucero@gmail.com>
2018-04-12 08:42:04 -0300

    adding menu

--- app/layout/index.controller.js
+++ app/layout/index.controller.js
@@ -9,9 +9,10 @@
     {
         var vm = this;
         vm.title = 'Angular from Scratch';
+        vm.selectedmenu = '';
         vm.menus = [
-                    {name:'chapter 2', url:"'C:/GITRepo/AngularFromScratch/app/chapter2/chaptertwo.html'"}, 
-                    {name:'chapter 3', url:"'C:/GITRepo/AngularFromScratch/app/chapter3/chapterthree.html'"}
+                    {name:'chapter 2', url:"include.html"}, 
+                    {name:'chapter 3', url:"C:/GITRepo/AngularFromScratch/app/chapter3/chapterthree.html"}
                     ];
     }
 
