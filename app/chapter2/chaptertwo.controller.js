732c142a02723eb5ed19229b7523a7f3120afe09
Roberto <robertoalucero@gmail.com>
2018-04-20 09:43:29 -0300

    clases and styles

--- app/chapter2/chaptertwo.controller.js
+++ app/chapter2/chaptertwo.controller.js
@@ -12,12 +12,32 @@
         vm.show = undefined;
         vm.hideAndShow = hideAndShow;
         vm.status = 'Show';
-        
+        vm.isError = false;
+        vm.isWarning = false;
+        vm.showError = showError;
+        vm.showWarning = showWarning;
+        vm.message = '';
+
+
         function hideAndShow()
         {
             vm.show = !vm.show;
             vm.status = (vm.show == true ? 'Hide' : 'Show');
-        };
+        }
 
+        function showError()
+        {
+            vm.message = "This is an error.";
+            vm.isError = true;
+            vm.isWarning = !v.isError;
+        }
+
+        
+        function showWarning()
+        {
+            vm.message = "This is a warning.";
+            vm.isWarning = true;
+            vm.isError = !v.isWarning;
+        }
     }
 })();
